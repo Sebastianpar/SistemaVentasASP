da8ee95c8a91efafe191fb3d36612042f926de0c
sebastianpar <jspc.95@hotmail.com>
2024-09-25 10:44:49 -0300

    preload of business form with the existing data and loading overlay

--- SistemaVenta.WebApp/wwwroot/js/vistas/Negocio_Index.js
+++ SistemaVenta.WebApp/wwwroot/js/vistas/Negocio_Index.js
@@ -1,1 +1,26 @@
-﻿
+﻿$(document).ready(function () {
+    $(".card-body").LoadingOverlay("show");
+    fetch("/Negocio/Obtener")
+        .then(response => {
+            $(".card-body").LoadingOverlay("hide");
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => {
+
+            console.log(responseJson)
+
+            if (responseJson.estado) {
+                const obj = responseJson.objeto
+                $("txtNumeroDocumento").val(obj.numeroDocumento)
+                $("txtRazonSocial").val(obj.nombre)
+                $("txtCorreo").val(obj.correo)
+                $("txtDireccion").val(obj.direccion)
+                $("txTelefono").val(obj.telefono)
+                $("txtImpuesto").val(obj.porcentajeImpuesto)
+                $("txtSimboloMoneda").val(obj.simboloMoneda)
+                $("imgLogo").attr("src", obj.urlLogo)
+            } else {
+                swal("Lo sentimos",responseJson.mensaje,"error")
+            }
+        })
+})
